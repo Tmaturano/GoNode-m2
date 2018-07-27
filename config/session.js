bb784310f15d243fe01b547472b90ef19e8935a0
Thiago Maturana <thiagomatu@hotmail.com>
2018-07-27 14:14:26 -0300

    Included the sequelize connect session package to handle the user session in the database instead of node server memory

--- config/session.js
+++ config/session.js
@@ -1,5 +1,12 @@
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const { sequelize } = require('../app/models');
+
 module.exports = {
   secret: 'snippetfy2018tmaturano', // para identificar unicamente a aplicação. Forma de criptografar a sessão do usuário baseado neste valor
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false, // will only create the session in the DB when the session is created
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
